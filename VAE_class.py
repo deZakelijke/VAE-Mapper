21976b8bfe05f0f2cdeebac4741c5abda3b872c4
Micha de Groot <git@michadegroot.nl>
2018-04-30 16:47:28 +0200

    Splitted some things in separate files, can now save model

---
+++ VAE_class.py
@@ -0,0 +1,64 @@
+# VAE class
+
+import numpy as np
+import torch
+import torch.utils.data
+from torch import nn, optim
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torchvision import datasets, transforms
+
+class VAE(nn.Module):
+    def __init__(self):
+        # Inherit from parent class
+        super().__init__()
+
+        self.flat_input_size = 6912 # 64 * 36 * 3
+        self.latent_dims = 8
+
+        # Encoding layers
+        self.conv1 = nn.Conv2d(3, 16, 3, stride=3, padding=1)
+        self.conv2 = nn.Conv2d(16, self.latent_dims, 3, stride=2, padding=1)
+        self.maxpool = nn.MaxPool2d(2)
+
+        # Decoding layers
+        self.deConv1 = nn.ConvTranspose2d(self.latent_dims, 16, 5, stride=3)
+        self.deConv2 = nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1)
+        self.deConv3 = nn.ConvTranspose2d(8, 3, 2, stride=2, padding=1)
+
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+
+    def encode(self, x):
+        h1 = self.maxpool(self.relu(self.conv1(x)))
+        h2 = self.maxpool(self.relu(self.conv2(h1)))
+        return h2, h2
+
+    # Read reparametrization trick again
+    # Seems to draw from normal dist with std 0.5 and mu=mu
+    def reparametrize(self, mu, logvar):
+        if self.training:
+            # Shouldnt the dist be drawn from N(0,1)?
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+
+    def decode(self, z):
+        h3 = self.relu(self.deConv1(z))
+        h4 = self.relu(self.deConv2(h3))
+        h5 = self.deConv3(h4)
+        return self.sigmoid(h5)
+
+
+    # encode and decode a data point
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 3, 64, 64))
+        z = self.reparametrize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
+
--- train_VAE.py
+++ train_VAE.py
@@ -9,7 +9,7 @@ from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 from VideoData import VideoData
-
+from VAE_class import VAE
 
 # argparsing
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
@@ -17,13 +17,17 @@ parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                             help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
                             help='number of epochs to train (default: 10)')
-                    # was --no-cude in original
 parser.add_argument('--cuda', action='store_true', default=False,
                             help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                             help='random seed (default: 1)')
 parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                             help='how many batches to wait before logging training status')
+parser.add_argument('--nr_images', type=int, default=1000, metavar='N',
+                            help='Number of images from the dataset that are used (defaut: 1000)')
+parser.add_argument('--save_path', type=str, default=None, metavar='P',
+                            help='Path to file to save model')
+
 args = parser.parse_args()
 args.cuda = args.cuda and torch.cuda.is_available()
 
@@ -32,16 +36,8 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-# get MNIST data
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-#train = datasets.MNIST('../data', train=True, download=True,
-#            transform=transforms.ToTensor(),
-#            batch_size=args.batch_size, shuffle=True, **kwargs)
-#test = datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-#            batch_size=args.batch_size, shuffle=True, **kwargs
-
-dataset = VideoData()
+dataset = VideoData(nr_images=args.nr_images)
 
 train_loader = torch.utils.data.DataLoader(dataset, 
                                            batch_size=args.batch_size, 
@@ -52,66 +48,17 @@ test_loader = torch.utils.data.DataLoader(dataset,
                                           shuffle=True,
                                           **kwargs)
 
-
-class VAE(nn.Module):
-    def __init__(self):
-        # Inherit from parent class
-        super().__init__()
-
-        self.flat_input_size = 6912 # 64 * 36 * 3
-        self.latent_dims = 8
-
-        # Encoding layers
-        self.conv1 = nn.Conv2d(3, 16, 3, stride=3, padding=1)
-        self.conv2 = nn.Conv2d(16, self.latent_dims, 3, stride=2, padding=1)
-        self.maxpool = nn.MaxPool2d(2)
-
-        # Decoding layers
-        self.deConv1 = nn.ConvTranspose2d(self.latent_dims, 16, 5, stride=3)
-        self.deConv2 = nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1)
-        self.deConv3 = nn.ConvTranspose2d(8, 3, 2, stride=2, padding=1)
-
-        self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
-
-
-    def encode(self, x):
-        h1 = self.maxpool(self.relu(self.conv1(x)))
-        h2 = self.maxpool(self.relu(self.conv2(h1)))
-        return h2, h2
-
-    # Read reparametrization trick again
-    # Seems to draw from normal dist with std 0.5 and mu=mu
-    def reparametrize(self, mu, logvar):
-        if self.training:
-            # Shouldnt the dist be drawn from N(0,1)?
-            std = logvar.mul(0.5).exp_()
-            eps = Variable(std.data.new(std.size()).normal_())
-            return eps.mul(std).add_(mu)
-        else:
-            return mu
-
-
-    def decode(self, z):
-        h3 = self.relu(self.deConv1(z))
-        h4 = self.relu(self.deConv2(h3))
-        h5 = self.deConv3(h4)
-        return self.sigmoid(h5)
-
-
-    # encode and decode a data point
-    def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 3, 64, 64))
-        z = self.reparametrize(mu, logvar)
-        return self.decode(z), mu, logvar
-
-
+#####
+# Class used to be here, moved to a separate file
 
 model = VAE().double()
 if args.cuda:
     model.cuda()
 optimizer = optim.Adam(model.parameters(), lr = 1e-2)
 
+
+# Maybe move functions somewhere else as well?
+# TODO?
 
 # Total loss function
 def loss_function(recon_x, x, mu, logvar):
@@ -181,3 +128,7 @@ for epoch in range(1, args.epochs + 1):
     sample = model.decode(sample).cpu()
     #save_image(sample.data.view(64, *size), 
     #           'results/sample_' + str(epoch) + '.png')
+
+    # Save model
+    if args.save_path:
+        torch.save(model, args.save_path)
